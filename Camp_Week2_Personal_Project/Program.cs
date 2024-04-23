7e666c034b3d64322279c1b32c08f181a9407931
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2024-04-23 17:10:59 +0900

    초기 커밋.

---
+++ Camp_Week2_Personal_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Camp_Week2_Personal_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
